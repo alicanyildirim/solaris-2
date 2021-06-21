51b175fc49ed0c50fb1b83575eab976df87521f8
Alican Yıldırım <alicany6m@gmail.com>
2021-06-21 04:05:33 +0300

    somewhat more clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,77 @@
 import React, { useLayoutEffect} from 'react';
 import './App.css';
-import Xreatures from "./Solaris-2.json";
+import Creatures from "./Solaris-2.json";
 import '../node_modules/react-vis/dist/style.css';
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 am4core.useTheme(am4themes_animated);
 function App() {
+  const addData = (status) => {
 
+  }
   const showData=()=>{
-    let cs: any = Xreatures;
-    let Creatures: any = cs[cs.length -1][1];
+    let cs: any = Creatures;
+    let creatures: any = cs[cs.length -1][1];
     let alive = {};
     let dead = {};
     let unknown = {};
-    for (let i = 0; i < Creatures.length; i++) {
-      const creature: any = Creatures[i].taxonomy;
-      const creature_status: any = Creatures[i].status;
+    for (let i = 0; i < creatures.length; i++) {
+      const tax: any = creatures[i].taxonomy;
+      const creature_status: any = creatures[i].status;
       if (creature_status === "alive") {
-          if (creature[0] in alive) {
-            if (creature[1] in alive[creature[0]]) {
-              if (creature[2] in alive[creature[0]][creature[1]] ) {
-                alive[creature[0]][creature[1]][creature[2]]++;
+          if (tax[0] in alive) {
+            if (tax[1] in alive[tax[0]]) {
+              if (tax[2] in alive[tax[0]][tax[1]] ) {
+                alive[tax[0]][tax[1]][tax[2]]++;
               } else {
-                alive[creature[0]][creature[1]][creature[2]] = 1;
+                alive[tax[0]][tax[1]][tax[2]] = 1;
               }
             } else {
-              alive[creature[0]][creature[1]] = {};
-              alive[creature[0]][creature[1]][creature[2]] = 1;
+              alive[tax[0]][tax[1]] = {};
+              alive[tax[0]][tax[1]][tax[2]] = 1;
             }
             
           } else {
-            alive[creature[0]] = {};
-            alive[creature[0]][creature[1]] = {};
-            alive[creature[0]][creature[1]][creature[2]] = 1;
+            alive[tax[0]] = {};
+            alive[tax[0]][tax[1]] = {};
+            alive[tax[0]][tax[1]][tax[2]] = 1;
           }
       } else if (creature_status === "dead") {
-          if (creature[0] in dead) {
-            if (creature[1] in dead[creature[0]]) {
-              if (creature[2] in dead[creature[0]][creature[1]] ) {
-                dead[creature[0]][creature[1]][creature[2]]++;
+          if (tax[0] in dead) {
+            if (tax[1] in dead[tax[0]]) {
+              if (tax[2] in dead[tax[0]][tax[1]] ) {
+                dead[tax[0]][tax[1]][tax[2]]++;
               } else {
-                dead[creature[0]][creature[1]][creature[2]] = 1;
+                dead[tax[0]][tax[1]][tax[2]] = 1;
               }
             } else {
-              dead[creature[0]][creature[1]] = {};
-              dead[creature[0]][creature[1]][creature[2]] = 1;
+              dead[tax[0]][tax[1]] = {};
+              dead[tax[0]][tax[1]][tax[2]] = 1;
             }
             
           } else {
-            dead[creature[0]] = {};
-            dead[creature[0]][creature[1]] = {};
-            dead[creature[0]][creature[1]][creature[2]] = 1;
+            dead[tax[0]] = {};
+            dead[tax[0]][tax[1]] = {};
+            dead[tax[0]][tax[1]][tax[2]] = 1;
           }
       } else if (creature_status === "unknown") {
-          if (creature[0] in unknown) {
-            if (creature[1] in unknown[creature[0]]) {
-              if (creature[2] in unknown[creature[0]][creature[1]] ) {
-                unknown[creature[0]][creature[1]][creature[2]]++;
+          if (tax[0] in unknown) {
+            if (tax[1] in unknown[tax[0]]) {
+              if (tax[2] in unknown[tax[0]][tax[1]] ) {
+                unknown[tax[0]][tax[1]][tax[2]]++;
               } else {
-                unknown[creature[0]][creature[1]][creature[2]] = 1;
+                unknown[tax[0]][tax[1]][tax[2]] = 1;
               }
             } else {
-              unknown[creature[0]][creature[1]] = {};
-              unknown[creature[0]][creature[1]][creature[2]] = 1;
+              unknown[tax[0]][tax[1]] = {};
+              unknown[tax[0]][tax[1]][tax[2]] = 1;
             }
             
           } else {
-            unknown[creature[0]] = {};
-            unknown[creature[0]][creature[1]] = {};
-            unknown[creature[0]][creature[1]][creature[2]] = 1;
+            unknown[tax[0]] = {};
+            unknown[tax[0]][tax[1]] = {};
+            unknown[tax[0]][tax[1]][tax[2]] = 1;
           }
       }
     }
@@ -78,52 +80,28 @@ function App() {
     // Log Time: new Date(Creatures[0][0]).toDateString()
     return {alive, dead, unknown}
   }
-
+  const mapData = ((status) => {
+    let arr: any = [];
+    for (let i = 0; i < Object.keys(status).length; i++) {
+        let obj: any = { "name": Object.keys(status)[i], "children": [] };  
+        const layer0: any = Object.values(status)[i];
+        for (let j = 0; j < Object.keys(layer0).length; j++) {
+          obj["children"].push({"name": Object.keys(layer0)[j], "children": []});
+          const layer1: any = Object.values(layer0)[j];
+          for (let z = 0; z < Object.keys(layer1).length; z++) {
+            obj["children"][j]["children"].push({"name": Object.keys(layer1)[z], "value": Object.values(layer1)[z]});
+          }
+        }
+        arr.push(obj);
+    }
+    return arr
+  });
   useLayoutEffect(() => {
     let {alive, dead, unknown} = showData();
     let chart: any = am4core.create("chartdiv", am4charts.TreeMap);
-    let aliveArr: any = [];
-    for (let i = 0; i < Object.keys(alive).length; i++) {
-        let obj: any = { "name": Object.keys(alive)[i], "children": [] }; 
-        
-        const layer0: any = Object.values(alive)[i];
-        for (let j = 0; j < Object.keys(layer0).length; j++) {
-          obj["children"].push({"name": Object.keys(layer0)[j], "children": []});
-          const layer1: any = Object.values(layer0)[j];
-          for (let z = 0; z < Object.keys(layer1).length; z++) {
-            obj["children"][j]["children"].push({"name": Object.keys(layer1)[z], "value": Object.values(layer1)[z]});
-          }
-        }
-        aliveArr.push(obj);
-    }
-    let deadArr: any = [];
-    for (let i = 0; i < Object.keys(dead).length; i++) {
-        let obj: any = { "name": Object.keys(dead)[i], "children": [] }; 
-        
-        const layer0: any = Object.values(dead)[i];
-        for (let j = 0; j < Object.keys(layer0).length; j++) {
-          obj["children"].push({"name": Object.keys(layer0)[j], "children": []});
-          const layer1: any = Object.values(layer0)[j];
-          for (let z = 0; z < Object.keys(layer1).length; z++) {
-            obj["children"][j]["children"].push({"name": Object.keys(layer1)[z], "value": Object.values(layer1)[z]});
-          }
-        }
-        deadArr.push(obj);
-    }
-    let unknownArr: any = [];
-    for (let i = 0; i < Object.keys(unknown).length; i++) {
-        let obj: any = { "name": Object.keys(unknown)[i], "children": [] }; 
-        
-        const layer0: any = Object.values(unknown)[i];
-        for (let j = 0; j < Object.keys(layer0).length; j++) {
-          obj["children"].push({"name": Object.keys(layer0)[j], "children": []});
-          const layer1: any = Object.values(layer0)[j];
-          for (let z = 0; z < Object.keys(layer1).length; z++) {
-            obj["children"][j]["children"].push({"name": Object.keys(layer1)[z], "value": Object.values(layer1)[z]});
-          }
-        }
-        unknownArr.push(obj);
-    }
+    const aliveArr = mapData(alive);
+    let deadArr = mapData(dead);
+    let unknownArr = mapData(unknown);
     chart.data = [
       {
       "name": "Alive",
